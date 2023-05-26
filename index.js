b1352b6193562765b4ca5edf14672fdfe76d427b
Zihadul Islam <zynscriptofficial@gmail.com>
2023-05-26 20:58:08 +0600

    server side complete

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const chefs = require("./data/chefs.json");
-
 app.use(cors());
 
 app.get("/", (req, res) => {
